66de4cdc3654290b64b21ed09fdfbc09cd603a46
Manel-93 <159416474+Manel-93@users.noreply.github.com>
2025-11-25 11:28:46 +0100

    Correction nom bdd/ Accès total à l'interface

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ const mysqlPool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'nahb_db',
+  database: process.env.DB_NAME || 'nahb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
